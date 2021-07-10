105c866269487831e4d41d102b3070cd20e201b8
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-07-10 15:29:21 +0200

    Update references

--- voc12/make_cls_labels.py
+++ voc12/make_cls_labels.py
@@ -1,5 +1,5 @@
 import argparse
-import voc12.data
+import data
 import numpy as np
 
 if __name__ == '__main__':
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     parser.add_argument("--voc12_root", required=True, type=str)
     args = parser.parse_args()
 
-    img_name_list = voc12.data.load_img_name_list(args.train_list)
-    img_name_list.extend(voc12.data.load_img_name_list(args.val_list))
-    label_list = voc12.data.load_image_label_list_from_xml(img_name_list, args.voc12_root)
+    img_name_list = data.load_img_name_list(args.train_list)
+    img_name_list.extend(data.load_img_name_list(args.val_list))
+    label_list = data.load_image_label_list_from_xml(img_name_list, args.voc12_root)
 
     d = dict()
     for img_name, label in zip(img_name_list, label_list):
